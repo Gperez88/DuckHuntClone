14ee57f6c7254ab46aa891e8b5e4d63b0fbf71ca
milkywayw <milkywayw@gmail.com>
2017-02-25 20:52:26 -0800

    Abstract out and generalize Duck's direction logic

--- core/src/com/ajrod/duckhunt/states/GameState.java
+++ core/src/com/ajrod/duckhunt/states/GameState.java
@@ -170,7 +170,7 @@ public class GameState extends State {
         sb.draw(statusBox, 200, 40, statusBox.getRegionWidth() * 3.125f, statusBox.getRegionHeight() * 2.5f);
         sb.draw(roundCount, 135, 102, roundCount.getRegionWidth() * 3.125f, roundCount.getRegionHeight() * 2.5f);
 
-        for (int i = 0; i < NUM_BIRD_ICONS; ++i) {
+        for (int i = 0; i < NUM_BIRD_ICONS; i++) {
             TextureRegion birdIcon = birdIcons.get(i);
          
             sb.draw(birdIcon, 290 + (birdIcon.getRegionWidth() * 3.125f + 3) * i, 62,
